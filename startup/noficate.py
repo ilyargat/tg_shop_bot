eb87737cc709e8e0e192b0bfd728d36882051b54
ilyargat <81382684+ilyargat@users.noreply.github.com>
2021-10-13 09:50:56 +0300

    Update noficate.py

--- startup/noficate.py
+++ startup/noficate.py
@@ -3,7 +3,7 @@ from data.data_edit import dating
 class Noficate:
     def __init__(self,bot,creator):
 
-        self.ADMIN=[1783366130]
+        self.ADMIN=[creator]
         self.bot=bot
         
     async def strike(self,dp):
